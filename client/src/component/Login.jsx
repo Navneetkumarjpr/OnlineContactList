a26173ea3e42e783b1f45153e0dc0c470c88dfe8
Navneet kumar <navaneetjpr@gmail.com>
2023-03-03 18:17:41 +0530

    four commit

--- client/src/component/Login.jsx
+++ client/src/component/Login.jsx
@@ -26,7 +26,7 @@ useEffect(() => {
         <div className="loginContainer">
           <h1>Welcome to the online Contact List Saving Website</h1>
           <h3>To use the Website easily, Please Login First</h3>
-           <GoogleLogin className='googleLoginButton'  clientId={process.env.REACT_APP_CLIENT_Id} isSignedIn={true} buttonText="Login" onAutoLoadFinished = {true} onSuccess={onLoginSuccess} onFailure={onLoginFailure} cookiePolicy={'single_host_origin'}></GoogleLogin>
+           <GoogleLogin className='googleLoginButton'  clientId={process.env.REACT_APP_CLIENT_Id} isSignedIn={true} buttonText="Login" onAutoLoadFinished = {true} onSuccess={onLoginSuccess} onFailure={onLoginFailure} ></GoogleLogin>
         </div>
     </div>
   )
