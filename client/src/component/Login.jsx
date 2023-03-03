dbd1b535c18571869914f240a8407c4c3d381dbf
Navneet kumar <navaneetjpr@gmail.com>
2023-03-03 17:58:04 +0530

    four commit

--- client/src/component/Login.jsx
+++ client/src/component/Login.jsx
@@ -2,7 +2,7 @@ import React,{useContext,useEffect} from 'react'
 import './login.css';
 
 import {GoogleLogin} from "react-google-login"
-import {clientId} from "../data/Data" 
+// import {clientId} from "../data/Data" 
 
 import { UserContext } from '../context/UserProvider';
 import { useHistory } from 'react-router-dom';
@@ -19,14 +19,14 @@ const onLoginFailure=()=>{
 }
 useEffect(() => {
   history.push('/');
-}, [])
+},[])
   //  console.log("clientID ",process.env.REACT_APP_CLIENT_Id);
   return (
     <div className='loginPage'>
         <div className="loginContainer">
           <h1>Welcome to the online Contact List Saving Website</h1>
           <h3>To use the Website easily, Please Login First</h3>
-           <GoogleLogin className='googleLoginButton'  clientId="166106136902-l64ef1ufmhamtsrtsalt17bi5e1tg3ck.apps.googleusercontent.com" isSignedIn={true} buttonText="Login" onAutoLoadFinished = {true} onSuccess={onLoginSuccess} onFailure={onLoginFailure} cookiePolicy={'single_host_origin'}></GoogleLogin>
+           <GoogleLogin className='googleLoginButton'  clientId={process.env.REACT_APP_CLIENT_Id} isSignedIn={true} buttonText="Login" onAutoLoadFinished = {true} onSuccess={onLoginSuccess} onFailure={onLoginFailure} cookiePolicy={'single_host_origin'}></GoogleLogin>
         </div>
     </div>
   )
