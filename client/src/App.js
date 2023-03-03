5a98bfcba4944b8c53cebc319a7d56e586b6e0a9
Navneet kumar <navaneetjpr@gmail.com>
2023-03-03 16:17:25 +0530

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,29 @@
+import './App.css';
+import React,{useContext} from 'react'
+import HomePage from './component/HomePage';
+import Login from './component/Login';
+import NewData from './component/NewData';
+import PageNotFound from './component/PageNotFound';
+import {Route,Switch} from "react-router-dom"
+import Header from './component/Header';
+import Update from './component/Update';
+import {UserContext} from "./context/UserProvider"
+function App() {
+  const {userLogged} = useContext(UserContext);
+  return (
+    <div className="App">    
+    {
+      userLogged!=null?<Header/>:""
+    }  
+      <Switch>
+         <Route exact path='/'>{userLogged!=null?<HomePage/>:<Login/>}</Route>
+         <Route path='/newData'>{userLogged!=null?<NewData/>:<Login/>}</Route>
+         
+         <Route path='/update/:id'>{userLogged!=null?<Update/>:<Login/>}</Route>
+         <Route path='*'><PageNotFound/></Route>
+       </Switch>
+    </div>
+  );
+}
+
+export default App;
